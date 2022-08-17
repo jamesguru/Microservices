53955d00f9bb0cb340b478b5680c647e7e793f9d
unknown <jameskagunga2022@gmail.com>
2022-08-17 00:03:35 -0700

    starting on auth service

---
+++ Auth/src/index.ts
@@ -0,0 +1,20 @@
+import express, { json, Request, Response } from 'express';
+
+
+const app = express();
+
+
+app.use(json())
+
+app.use((err:Error, req:Request, res:Response) => {
+
+    if(err.message){
+
+        console.log(err.message);
+    }
+})
+
+app.listen(4000,() => {
+
+    console.log('app is running on port 4000')
+})
